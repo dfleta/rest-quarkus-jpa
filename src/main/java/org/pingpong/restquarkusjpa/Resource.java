27e197625f460b771500335aa6afe59a0840c069
dfleta <gelpiorama@gmail.com>
2021-07-01 18:31:53 +0200

    feat(Resource): Implementar dependencia a servicio y endpoint

--- src/main/java/org/pingpong/restquarkusjpa/Resource.java
+++ src/main/java/org/pingpong/restquarkusjpa/Resource.java
@@ -1,16 +1,25 @@
 package org.pingpong.restquarkusjpa;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+import org.pingpong.restquarkusjpa.service.ServiceItem;
+
+@Path("/")
 public class Resource {
 
+    @Inject
+    ServiceItem service;
+
     @GET
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/itemcrudos")
+    public String wellcome() {
+        return "CRUD de Items!";
     }
 }
--- src/test/java/org/pingpong/restquarkusjpa/ResourceTest.java
+++ src/test/java/org/pingpong/restquarkusjpa/ResourceTest.java
@@ -1,21 +1,50 @@
 package org.pingpong.restquarkusjpa;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import javax.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+
 @QuarkusTest
 public class ResourceTest {
+    
+    @Inject
+    Resource resources;
+
+
+    /**
+	 * Implementa una clase Resources que
+	 * añada una API REST a nuestra app.
+	 * Injecta el servicio en Resources.
+     */
 
     @Test
-    public void testHelloEndpoint() {
+    public void test_injeccion() {
+        Assertions.assertThat(resources.service).isNotNull();
+    }
+
+    /**
+     * En el endpoint /itemcrudos
+     * recibimos un TEXT con el mensaje
+     * "CRUD de Items!""
+     */
+
+    @Test
+    public void test_wellcome() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .contentType(ContentType.TEXT)
+        .when()
+            .get("/itemcrudos")
+        .then()
+            .statusCode(200)
+            .body(is("CRUD de Items!"));
     }
 
 }
